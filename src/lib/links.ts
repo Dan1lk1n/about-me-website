46f78901fe0599e3c6909bfa08a6b95f5e1275a9
Данил Петров <petrov@apeek.ru>
2026-04-03 22:44:55 +0500

    change github link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
-  telegram: "https://t.me/daniilbacardi",
-  email: "mailto:daniilbacardi@gmail.com",
-  hhru: "https://hh.ru/resume/daniilbacardi",
-  githubSite: "https://github.com/daniilbacardi/my-site",
+  telegram: "https://t.me/Dan1lk1n",
+  email: "mailto:danildze101@yandex.ru",
+  hhru: "https://ufa.hh.ru/resume/d81fffd8ff105029ba0039ed1f6d596f645750",
+  githubSite: "https://github.com/Dan1lk1n/about-me-website",
 };
